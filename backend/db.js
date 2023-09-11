a41449efa65455707b1754dd4b0cbe8665af949d
Neha Bulakhe <nehabulakhe0922@gmail.com>
2023-09-11 17:55:24 +0530

    connected to mongoose and checked on thunderclient

---
+++ backend/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://localhost:27017";
+
+const connectToMongo = async () => {
+ mongoose.connect(mongoURI, await console.log("Connected to mongo `Successful`")
+    );
+}
+
+module.exports = connectToMongo;
+
+// const mongoose =require ('mongoose');
+
+
+// const mongoURI="mongodb://localhost:27017";
+
+// const connectToMongo=()=>{
+//     mongoose.connect(mongoURI,()=>{
+//         console.log("connected successfully");
+//     })
+// }
+
+// module.exports= connectToMongo;
